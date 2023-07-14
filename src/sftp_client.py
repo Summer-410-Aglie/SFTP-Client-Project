5f13be5f6a5f8cb2b04cbfc76ddb9407a2b82206
BlackJesusN4 <86698477+BlackJesusN4@users.noreply.github.com>
2023-07-14 00:55:39 -0700

    added sftp_client object and login method

---
+++ src/sftp_client.py
@@ -0,0 +1,55 @@
+import pysftp
+
+class SFTPClient:
+    """ A wrapper class for SFTP Client
+    """    
+    def __init__(
+            self,
+            host_name: str,
+            user_name: str,
+            password: str,
+    ) -> None:
+        """Constructs the necessary data for the SFTP client object.
+
+        :param host_name: host name or IP address
+        :type host_name: str
+        :param user_name: user name for the host id
+        :type user_name: str
+        :param password: password for the user name
+        :type password: str
+        """          
+
+        self.host_name: str = host_name
+        self.user_name: str = user_name
+        self.password: str = password
+        pass
+
+    def connect(self) -> bool:
+        """Establishes the SFTP connection
+
+        :return: connection was successful or not
+        :rtype: bool
+        """
+        try:      
+            self.connection = pysftp.Connection(host=self.host_name, username=self.user_name, password=self.password)
+        except Exception as e:
+            print(str(e))
+            return False
+        
+        return True
+        pass
+
+    def close(self) -> bool:
+        """"Closes the established SFTP connection
+
+        :return: disconnection was successful or not
+        :rtype: bool
+        """
+        try:        
+            self.connection.close()
+        except Exception as e:
+            print(str(e))
+            return False
+        
+        return True
+        pass
